6f4bc986afe9bd1d741031b89c6bdc0c3bf79caf
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-11-26 05:01:38 +0000

    Done.

--- models/Question.js
+++ models/Question.js
@@ -4,6 +4,19 @@ const db = require("../config/db")
 
 class Question{
 
+  static CreateTable(){
+    const sql = `CREATE TABLE questions(
+                id INTEGER PRIMARY KEY,
+                content TEXT
+                  )`
+
+     return new Promise(function(resolve){
+       db.run(sql, function(){
+         resolve('Question Table Created')
+       })
+     })
+  }
+
 }
 
 
